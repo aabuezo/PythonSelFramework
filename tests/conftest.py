268aa709b68b15177426a69edbd3db8afda4c6b3
Alejandro A. Buezo <aabuezo@gmail.com>
2024-08-14 13:11:15 -0300

    added browser_name command line option to py.test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,26 @@ import pytest
 from selenium import webdriver
 
 
+# getting browser_name option from CMD
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome", help="browser_name: chrome or safari"
+    )
+
 @pytest.fixture(scope="class")
 def setup(request): # request is kind of a default parameter
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument("headless")
-    # driver = webdriver.Chrome(options=chrome_options)
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(2)
+
+    # get browser_name from CMD
+    browser_name = request.config.getoption("browser_name")
+
+    if browser_name == "chrome":
+        driver = webdriver.Chrome()
+    elif browser_name == "safari":
+        driver = webdriver.Safari()
+    
+    driver.implicitly_wait(5)
     driver.get("https://rahulshettyacademy.com/angularpractice/#")
-    # driver.maximize_window()
+    driver.maximize_window()
     request.cls.driver = driver # to pass the driver to the test class
     yield driver
     driver.close()
